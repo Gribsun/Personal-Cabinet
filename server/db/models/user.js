b7b37c1bc5a7fe33ad879e1a20a0dfe3ecfb4898
Anton <rednight-red@mail.ru>
2022-08-04 15:35:29 +0300

    Database connection

---
+++ server/db/models/user.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+    }
+  }
+  User.init({
+    userName: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
